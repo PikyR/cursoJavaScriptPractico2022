fa63dc12037d2a6acf9349a08873e4b2273cab9a
pikyr <pikytdv@gmail.com>
2022-08-13 17:27:37 -0300

    Fin de clase 11 - Escribiendo HTML desde JS

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,3 +13,28 @@ console.log({
 });
 
 
+// title
+title.innerText = 'El Titulo JS';
+
+// Por cuestiones de seguridad no es recomendable utilizar .innerHTML
+// title.innerHTML = 'El Titulo <br> JS'; 
+
+title.getAttribute('class'); // main-title
+title.setAttribute('class', 'main-title--underline');
+
+title.classList.add('main-title');
+title.classList.remove('main-title--underline');
+// title.classList.toggle('main-title');
+// title.classList.contains('main-title');
+
+/***************/
+
+// input
+input.value = 'Value desde JS';
+
+// document
+const image = document.createElement('img');
+image.setAttribute('src', 'https://via.placeholder.com/150');
+
+tercerParrafo.innerHTML = '';
+tercerParrafo.appendChild(image);
