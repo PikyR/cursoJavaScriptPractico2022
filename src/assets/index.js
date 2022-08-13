9fac5044c6c132b2b08cb8a2f0fc1a67624815f7
pikyr <pikytdv@gmail.com>
2022-08-13 16:33:32 -0300

    Fin de clase 10 - Leyendo HTML desde JS

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,15 @@
-alert('Coso')
+const title = document.querySelector('h1');
+const parrafo = document.querySelector('p');
+const segundoParrafo = document.querySelector('.segundoParrafo');
+const tercerParrafo = document.querySelector('#tercerParrafo');
+const input = document.querySelector('input');
+
+console.log({
+  title,
+  parrafo,
+  segundoParrafo,
+  tercerParrafo,
+  input
+});
+
+
