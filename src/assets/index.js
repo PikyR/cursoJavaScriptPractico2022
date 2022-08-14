9cad1282497070b9a084887ec5a1231774836fd2
pikyr <pikytdv@gmail.com>
2022-08-13 22:32:12 -0300

    Fin de clase 13 - addEventListener

--- src/assets/index.js
+++ src/assets/index.js
@@ -41,12 +41,20 @@
 
 
 /* Clase 12 */
-
+// suma
 const title = document.querySelector('.main-title');
 const input1 = document.querySelector('#input-1');
 const input2 = document.querySelector('#input-2');
 const btnCalc = document.querySelector('#btn-sumar');
 const resultContainer = document.querySelector('#resultContainer');
+// resta
+const form = document.querySelector('#resta')
+const restaInput1 = document.querySelector('#resta__input-1')
+const restaInput2 = document.querySelector('#resta__input-2')
+const restaResultContainer = document.querySelector('#resultContainerResta')
+
+// Solo poner el nombre de la función! 
+btnCalc.addEventListener('click', sumar); 
 
 function sumar() {
   const num1 = parseInt(input1.value);
@@ -54,4 +62,16 @@ function sumar() {
   const result = num1 + num2;
 
   resultContainer.innerText = `${num1} + ${num2} = ${result}`
+}
+
+form.addEventListener('submit', restar);
+
+function restar(event) {
+  event.preventDefault();
+
+  const num1 = parseInt(restaInput1.value);
+  const num2 = parseInt(restaInput2.value);
+  const result = num1 - num2;
+
+  restaResultContainer.innerText = `${num1} - ${num2} = ${result}`;
 }
